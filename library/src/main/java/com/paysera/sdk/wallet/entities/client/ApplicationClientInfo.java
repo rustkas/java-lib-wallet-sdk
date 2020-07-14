d618eb481653c438a678b948b87d55fddbfea72e
GIlbert Parreno <gilbertparreno95@gmail.com>
2020-07-14 14:44:20 +0800

    Extend ApplicationClientInfo

--- library/src/main/java/com/paysera/sdk/wallet/entities/client/ApplicationClientInfo.java
+++ library/src/main/java/com/paysera/sdk/wallet/entities/client/ApplicationClientInfo.java
@@ -1,11 +1,29 @@
 package com.paysera.sdk.wallet.entities.client;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ApplicationClientInfo {
     private String title;
     private String os;
     private String model;
     private String imei;
     private String deviceId;
+    @SerializedName("rooted")
+    private Boolean isRooted;
+    @SerializedName("simulator")
+    private Boolean isSimulator;
+    private String ipIpv4;
+    private String ipIpv6;
+    private String wifiSsid;
+    private String wifiMac;
+    private String systemLanguage;
+    private String simCountry;
+    private String simCarrier;
+    private String pseudoUniqueId;
+    private String androidId;
+    private String androidGfsid;
+    private String androidPhoneFingerprint;
+    private String webviewUserAgent;
 
     public String getTitle() {
         return title;
@@ -45,5 +63,117 @@ public class ApplicationClientInfo {
 
     public void setDeviceId(String deviceId) {
         this.deviceId = deviceId;
+    }
+
+    public Boolean getRooted() {
+        return isRooted;
+    }
+
+    public void setRooted(Boolean rooted) {
+        isRooted = rooted;
+    }
+
+    public Boolean getSimulator() {
+        return isSimulator;
+    }
+
+    public void setSimulator(Boolean simulator) {
+        isSimulator = simulator;
+    }
+
+    public String getIpIpv4() {
+        return ipIpv4;
+    }
+
+    public void setIpIpv4(String ipIpv4) {
+        this.ipIpv4 = ipIpv4;
+    }
+
+    public String getIpIpv6() {
+        return ipIpv6;
+    }
+
+    public void setIpIpv6(String ipIpv6) {
+        this.ipIpv6 = ipIpv6;
+    }
+
+    public String getWifiSsid() {
+        return wifiSsid;
+    }
+
+    public void setWifiSsid(String wifiSsid) {
+        this.wifiSsid = wifiSsid;
+    }
+
+    public String getWifiMac() {
+        return wifiMac;
+    }
+
+    public void setWifiMac(String wifiMac) {
+        this.wifiMac = wifiMac;
+    }
+
+    public String getSystemLanguage() {
+        return systemLanguage;
+    }
+
+    public void setSystemLanguage(String systemLanguage) {
+        this.systemLanguage = systemLanguage;
+    }
+
+    public String getSimCountry() {
+        return simCountry;
+    }
+
+    public void setSimCountry(String simCountry) {
+        this.simCountry = simCountry;
+    }
+
+    public String getSimCarrier() {
+        return simCarrier;
+    }
+
+    public void setSimCarrier(String simCarrier) {
+        this.simCarrier = simCarrier;
+    }
+
+    public String getPseudoUniqueId() {
+        return pseudoUniqueId;
+    }
+
+    public void setPseudoUniqueId(String pseudoUniqueId) {
+        this.pseudoUniqueId = pseudoUniqueId;
+    }
+
+    public String getAndroidId() {
+        return androidId;
+    }
+
+    public void setAndroidId(String androidId) {
+        this.androidId = androidId;
+    }
+
+    public String getAndroidGfsid() {
+        return androidGfsid;
+    }
+
+    public void setAndroidGfsid(String androidGfsid) {
+        this.androidGfsid = androidGfsid;
+    }
+
+    public String getAndroidPhoneFingerprint() {
+        return androidPhoneFingerprint;
+    }
+
+    public void setAndroidPhoneFingerprint(String androidPhoneFingerprint) {
+        this.androidPhoneFingerprint = androidPhoneFingerprint;
+    }
+
+    public String getWebviewUserAgent() {
+        return webviewUserAgent;
+    }
+
+    public void setWebviewUserAgent(String webviewUserAgent) {
+        this.webviewUserAgent = webviewUserAgent;
     }
 }
