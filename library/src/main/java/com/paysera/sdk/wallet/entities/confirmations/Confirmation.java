ef713132eb77e44ff4b76bada1396503ac69a68e
Lukas Šivickas <l.sivickas@gmail.com>
2020-08-10 13:07:02 +0300

    Append additional fields to Confirmation

--- library/src/main/java/com/paysera/sdk/wallet/entities/confirmations/Confirmation.java
+++ library/src/main/java/com/paysera/sdk/wallet/entities/confirmations/Confirmation.java
@@ -6,6 +6,10 @@ public class Confirmation {
     private String status;
     private Integer userId;
     private ConfirmationProperties properties;
+    private String countryCode;
+    private String city;
+    private String browser;
+    private String os;
 
     public String getChallengeId() {
         return challengeId;
@@ -46,4 +50,20 @@ public class Confirmation {
     public void setProperties(ConfirmationProperties properties) {
         this.properties = properties;
     }
+
+    public String getCountryCode() { return countryCode; }
+
+    public void setCountryCode(String countryCode) { this.countryCode = countryCode; }
+
+    public String getCity() { return city; }
+
+    public void setCity(String city) { this.city = city; }
+
+    public String getBrowser() { return browser; }
+
+    public void setBrowser(String browser) { this.browser = browser; }
+
+    public String getOs() { return os; }
+
+    public void setOs(String os) { this.os = os; }
 }
